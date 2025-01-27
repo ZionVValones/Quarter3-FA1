3617b782ec3a6b9ce2283d96fb6399a59856dc3d
ZionVValones <zion.valones@smc.pshs.edu.ph>
2025-01-27 19:02:07 +0800

    Update script1.js
    added the modified code

--- script1.js
+++ script1.js
@@ -1,17 +1,10 @@
-<script>
-    var nickname = prompt("Enter nickname: ", "0");
-    var height = prompt("Enter height in inches: ", "0" );
-    var weight = prompt("Enter weight in kilograms: ", "0");
+let nickname = "Zion";
+let heightInches = 62;
+let weightKg = 50;
 
-    var inch = height % 12;
-    Math.round(inch);
-    var foot = (height - inch) / 12;
-    Math.round (foot);
-    var pounds = weight = 2.205;
-    Math.round(pounds);
+let feet = Math.round(heightInches / 12);
+let inches = heightInches % 12;
+let weightLbs = (weightKg * 2.20462);
+Math.round(weightLbs);
 
-
-    alert("Name: " + nickname + "<\br>" + "Height: " + foot "'" + "<\br>" + "Weight: " + pounds + " lbs");
-
-
-</script>
+alert(`Name: ${nickname}\nHeight: ${feet}'${inches}\"\nWeight: ${weightLbs} lbs`);
